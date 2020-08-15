81d556220dad52b978639e02584ca5c1ef719eef
ene <nfgomez08@misena.edu.co>
2020-08-15 09:38:15 -0500

    js de parte del enes

---
+++ js/jsenlen.js
@@ -0,0 +1,13 @@
+function nuevaCaja(){ 
+    var seccion = document.querySelector(".section");
+
+
+    var botones1= document.createElement("button");
+
+    seccion.appendChild(botones1);
+    
+    botones1.className="boton";
+
+    botones1.innerHTML = "CLICKME";
+
+}
